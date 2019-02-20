e35b137ecbc99338218ad8e5de5d55db9b4e48b2
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-02-20 11:25:22 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo apt install samba cups cups-pdf zip remmina remmina-plugin* openssh-server skypeforlinux virtualbox-5.1 virtualbox-qt virtualbox-dkms virtualbox-guest-additions-iso hplip hplip-gui
+sudo add-apt-repository universe && sudo add-apt-repository multiverse && sudo apt-get update && sudo apt install samba cups cups-pdf zip remmina remmina-plugin* openssh-server skypeforlinux virtualbox-5.1 virtualbox-qt virtualbox-dkms virtualbox-guest-additions-iso hplip hplip-gui
