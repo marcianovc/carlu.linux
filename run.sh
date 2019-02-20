82a1f130fea54a6773ed1bfa4de3fcce56248b07
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-02-20 11:04:15 -0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+sudo apt install samba cups cups-pdf zip remmina remmina-plugin* openssh-server skypeforlinux virtualbox-5.1 virtualbox-qt virtualbox-dkms virtualbox-guest-additions-iso hplip hplip-gui
