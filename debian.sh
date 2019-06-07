1a0ec25bf28d7ac976b246326e6a9a26ffd70ee1
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-06-07 11:48:28 -0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 user=`egrep '1[0-9]{3}' /etc/passwd | cut -d \: -f1`
+cd /home/$user/
 if [ ! -e /etc/apt/sources.list.d/stretch-backports.list ]
 then
     echo 'deb http://ftp.debian.org/debian stretch-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
 fi
 apt update
 apt install -t stretch-backports remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice cups-pdf zip hplip* -y
-wget https://go.skype.com/skypeforlinux-64.deb /home/$user/
+wget https://go.skype.com/skypeforlinux-64.deb
 dpkg -i /home/$user/skypeforlinux-64.deb
-cd /home/$user/
 wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1wgiUbBvRV8VwNmxjXRXVAo8zSwPvNFG_' -O smartclientLinux.zip
 unzip smartclientLinux.zip && rm smartclientLinux.zip && chown $user:$user smartclientLinux/ && chmod -R 777 smartclientLinux/ && cd smartclientLinux && rm totvs.desktop
 echo "[Desktop Entry]" >> totvs.desktop
