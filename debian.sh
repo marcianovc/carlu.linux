27bbf2403addfc9a0ad39a7f3d1d26dd9c3b3866
Eduardo Althaus <41025693+eduardoalthaus@users.noreply.github.com>
2019-06-08 08:52:23 -0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -6,7 +6,7 @@ then
     echo 'deb http://ftp.debian.org/debian stretch-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
 fi
 apt update
-apt install -t stretch-backports remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice cups-pdf zip hplip* apt-transport-https aptitude htop -y
+apt install -t stretch-backports remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice cups-pdf zip hplip* apt-transport-https aptitude htop gzip -y
 wget https://go.skype.com/skypeforlinux-64.deb
 dpkg -i skypeforlinux-64.deb
 wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1wgiUbBvRV8VwNmxjXRXVAo8zSwPvNFG_' -O smartclientLinux.zip
@@ -29,4 +29,5 @@ chmod +x totvs.desktop && chmod 777 totvs.desktop && mv totvs.desktop /usr/share
 dpkg --add-architecture i386 && apt update
 aptitude install gcc-multilib lib32z1 lib32ncurses5 libglib2.0-0:i386 libsm6:i386 libxrender1:i386 libxext6:i386 -y
 apt --fix-broken install -y
+git clone https://github.com/eduardoalthaus/hddtest && cd hddtest && chmod +x run.sh && ./run.sh
 exit
